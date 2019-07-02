747c20e114a271e967b903a822a664757908bc3d
lmh96 <loganhill23@gmail.com>
2019-07-01 21:54:26 -0400

    set up results page, and account settings / home pages

--- src/desktop/Account.js
+++ src/desktop/Account.js
@@ -2,12 +2,81 @@ import React from "react";
 
 import "./css/Account.css";
 
-function Account(props) {
-    return(
-        <div>
+class Account extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePage: "home",
+        }
+    }
 
-        </div>
-    );
+    handleStateChange = event => {
+        let { name, value } = event.target;
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    render() {
+        let arr = [];
+        for (let i = 0; i < 10; i++) {
+            arr.push(
+                <div key={i} className="past-result">
+                    <img className="past-result-image" src="https://i2.wp.com/josephmallozzi.com/wp-content/uploads/2018/01/159.jpg?ssl=1" alt="comic-cover">
+
+                    </img>
+                    <h2 className="past-result-title">
+                        ResultTitle
+                            </h2>
+                    <h3 className="past-result-year">
+                        (****-****)
+                            </h3>
+                </div>
+            )
+        }
+        return (
+            <div className="account-content">
+                <section className="side-panel">
+                    <button className={"account-nav-btn" + (this.state.activePage === "home" ? " active-account-nav" : "")} onClick={this.handleStateChange} name="activePage" value={"home"}>
+                        Home
+                    </button>
+                    <button className={"account-nav-btn" + (this.state.activePage === "settings" ? " active-account-nav" : "")} onClick={this.handleStateChange} name="activePage" value={"settings"}>
+                        Settings
+                    </button>
+                </section>
+                {console.log(this.state.activePage)}
+                {this.state.activePage === "home" ?
+                    <div className="home-content">
+                        <h2 className="home-title">Like History</h2>
+                        {arr.map(item => (
+                            item
+                        ))}
+                    </div>
+                    :
+                    <div className="settings-content">
+                        <form class="account-settings-form">
+                            <img className="account-settings-user-image" src="https://www.loginradius.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="user" />
+                            <h3 className="account-settings-select-image">Select user image</h3>
+
+                            <h3 className="account-settings-label"> Change Name: </h3>
+                            <input className="account-settings-input"></input>
+
+                            <h3 className="account-settings-label">Change Email: </h3>
+                            <input className="account-settings-input"></input>
+
+                            <h3 className="account-settings-label">New Password: </h3>
+                            <input className="account-settings-input"></input>
+
+                            <h3 className="account-settings-label">Confirm Password: </h3>
+                            <input className="account-settings-input"></input>
+
+                            <button className="account-settings-save-btn">Save</button>
+                        </form>
+                    </div>
+                }
+            </div>
+        );
+    }
 }
 
 export default Account;
--- src/desktop/Results.js
+++ src/desktop/Results.js
@@ -1,13 +1,49 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 
 import "./css/Results.css";
 
-function Results(props) {
-    return(
-        <div>
+class Results extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
 
-        </div>
-    );
+    render() {
+        let arr = [];
+        for (let i = 0; i < 10; i++) {
+            arr.push(
+                <h4 key={i} className="history-result">Title</h4>
+            )
+        }
+
+        return (
+            <div className="results-content">
+                <section className="results-history">
+                    <h2 className="history-title">past results</h2>
+                    {arr.map(item => (
+                        item
+                    ))}
+                </section>
+                <div className="current-result">
+                    <img className="current-cover" src="https://i2.wp.com/josephmallozzi.com/wp-content/uploads/2018/01/159.jpg?ssl=1" alt="cover" />
+                    <h2 className="current-title">Title: (****-****)</h2>
+                    <h4 className="current-run-length">### issues</h4>
+                    <h4 className="current-writer">Run Writer</h4>
+                    <h4 className="current-artist">Run Artist</h4>
+                    <h4 className="current-description-label">Description: </h4>
+                    <p className="current-description">enter stuff here</p>
+
+                    <button className="dislike-btn"><FontAwesomeIcon icon={faThumbsUp}/></button>
+                    <button className="like-btn"><FontAwesomeIcon icon={faThumbsDown}/></button>
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default Results;

67e60e1ef1f0d4cbe7e57f78f138b51ccb0b79a9
lmh96 <loganhill23@gmail.com>
2019-07-19 22:12:41 -0400

    checking mongo in deployed state

--- server.js
+++ server.js
@@ -25,12 +25,22 @@ require("./routes/apiRoutes")(app);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/build/index.html'));
 });
-// setInterval(function() {
-//     http.get("");
-// }, 300000);
+
+setInterval(function() {
+    http.get("https://recommendersite.herokuapp.com/");
+}, 300000);
+
 let user = "heroku_2psjs7x5";
 let pass = "doesthiswork1";
-// mongoose.connect("mongodb://" + user + ":" + pass + "@ds347367.mlab.com:47367/heroku_2psjs7x5", {useNewUrlParser: true});
+let URI = "mongodb://" + user + ":" + pass + "@ds347367.mlab.com:47367/heroku_2psjs7x5";
+console.log(URI);
+mongoose.connect("mongodb://localhost/recommender", {useNewUrlParser: true}, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Mongo loaded succesfully!");
+    }
+});
 
 app.listen(PORT, function () {
     console.log(
